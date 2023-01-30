1f28b3ab24665f0920ab651d8cb900ff2caab03a
NyshMysh <killer.bir007@gmail.com>
2023-01-30 16:49:52 +0300

    implemented a full-fledged update, but there was an error with the save paths in docker

--- app/Http/Controllers/Main/UpdateController.php
+++ app/Http/Controllers/Main/UpdateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Main;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Courses\UpdateRequest;
 use App\Models\Courses;
+use App\Models\Material;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -13,8 +14,20 @@ class UpdateController extends Controller
    public function __invoke(UpdateRequest $request, Courses $courses)
    {
        $data = $request->validated();
-       Storage::put('/materials', $data['materials']);
-       $courses->update($data);
+       $materials = Material::where('courses_id', $courses->id)->get();
+       foreach ($materials as $material) {
+           Storage::delete($material->material);
+       }
+       $new_courses = $courses->update($data);
+       foreach ($request->file('materials') as $material) {
+           $imageName = $data['title'].'-image-'.time().rand(1, 10000).'.'.$material->extension();
+           $material->move(public_path('storage/materials'), $imageName);
+           Material::create([
+               'courses_id' => $courses->id,
+               'material' => $imageName
+           ]);
+       }
+
        return view('main.show', compact('courses'));
    }
 }
